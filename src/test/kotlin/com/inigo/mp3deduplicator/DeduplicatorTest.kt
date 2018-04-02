1a130b56da5910555044cd18619bb7379e7e29d2
inigodm <inigo.delgado@gmail.com>
2018-04-02 11:07:47 +0200

    Added a test for testing a directory with a single song and done a little refactoring.
    In this moment we are able to see some things:
    
    - The method signature of the 'duplicate' method does not fit with the purpose of the method (it must return a list of duplicated songs because they can be more than one)
    
    - Now it depends on external things like the resources directory.
    
    I have to change both things and I will do it through the next refactoring

--- src/test/kotlin/com/inigo/mp3deduplicator/DeduplicatorTest.kt
+++ src/test/kotlin/com/inigo/mp3deduplicator/DeduplicatorTest.kt
@@ -1,14 +1,23 @@
 package com.inigo.mp3deduplicator
 
+import com.inigo.mp3deduplicator.data.SongInfo
 import org.junit.Assert
 import org.junit.Test
 
 class DeduplicatorTest {
     @Test
     fun testVoidPath(){
+        assertPath("", SongInfo())
+    }
+
+    @Test
+    fun testOneSongDirectory(){
+        assertPath("/home/inigo/projectsgit/mp3deduplicator/src/test/resources/one", SongInfo())
+    }
+
+    fun assertPath(path: String, expectedSongInfo: SongInfo){
         var dup = Deduplicator()
-        var si = dup.deduplicate("")
-        Assert.assertEquals("", si.title)
-        Assert.assertEquals("", si.path)
+        var si = dup.deduplicate(path)
+        Assert.assertEquals(expectedSongInfo, si)
     }
 }
